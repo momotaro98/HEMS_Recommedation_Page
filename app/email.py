fd99b9fb98902a05e93ce73587f78ef2fdee6cba
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-09-06 22:47:55 +0900

    fix for SendGrid

--- app/email.py
+++ app/email.py
@@ -9,11 +9,12 @@ from . import mail
 
 def send_async_email(app, msg, sg=None):
     with app.app_context():
-        if sg:
-            # sg.send(msg)
-            response = sg.client.mail.send.post(request_body=mail.get())
-        else:
-            mail.send(msg)
+        mail.send(msg)
+
+
+def send_async_email_with_sendgrid(app, sg):
+    with app.app_context():
+        response = sg.client.mail.send.post(request_body=mail.get())
 
 
 def send_email(to, subject, template, **kwargs):
@@ -25,21 +26,21 @@ def send_email(to, subject, template, **kwargs):
         '''
         sg = sendgrid.SendGridClient(app.config['SENDGRID_USERNAME'],
                                      app.config['SENDGRID_PASSWORD'])
-        '''
-        sg = sendgrid.SendGridAPIClient(apikey=app.config['SENDGRID_API_KEY'])
-        from_email = Email("test@example.com")
-        subject = "Hello World from the SendGrid Python Library!"
-        to_email = Email(to)
-        content = Content("text/plain", body)
-        mail = Mail(from_email, subject, to_email, content)
-        '''
         msg = sendgrid.Mail()
         msg.add_to(to)
         msg.set_subject(subject)
         msg.set_text(body)
         msg.set_from('Doe John')
         '''
-        thr = Thread(target=send_async_email, args=[app, msg, sg])
+
+        sg = sendgrid.SendGridAPIClient(apikey=app.config['SENDGRID_API_KEY'])
+        from_email = Email("test@example.com")
+        subject = "Hello World from the SendGrid Python Library!"
+        to_email = Email(to)
+        content = Content("text/plain", body)
+        mail = Mail(from_email, subject, to_email, content)
+        thr = Thread(target=send_async_email_with_sendgrid, args=[app, sg])
+
     else:
         msg = Message(app.config['APP_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                       sender=app.config['APP_MAIL_SENDER'], recipients=[to])

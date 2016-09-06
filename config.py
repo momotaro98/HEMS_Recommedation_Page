c578635fcfb8ee59beaa1dcf9bf17051d3e589e5
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-09-06 18:10:45 +0900

    fix pep8 and test ignore __pycache__ dir

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_RECOMMEND_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_RECOMMEND_DATABASE_URL') \
+        or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
 class ProductionConfig(Config):

8830133c282b9bdd9e422d3bd17588189d478733
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-09-07 21:29:38 +0900

    add demo page

--- app/static/js/graphs.js
+++ app/static/js/graphs.js
@@ -4,7 +4,7 @@
 	  labels: settemp_lables,
 	  datasets: [
 		{
-		  label: '利用率(%)',
+		  label: '設定温度使用割合[%]',
 		  hoverBackgroundColor: "rgba(255,99,132,0.3)",
 		  data: settemp_data,
 		}
@@ -33,10 +33,10 @@
 (function(){
 	// 運転時間合計
 	var totaltime_data_out = {
-	  labels: time_lables,
+	  labels: totaltime_lables,
 	  datasets: [
 		{
-		  label: '運転時間(時間)',
+		  label: '運転時間[時間]',
 		  hoverBackgroundColor: "rgba(255,99,132,0.3)",
 		  data: totaltime_data,
 		}
@@ -59,5 +59,36 @@
 	  options: options  //オプション設定
 
 	});
+}());
 
+
+(function(){
+	// 単位時間当たりの使用率グラフ
+	var perhour_data_out = {
+	  labels: perhour_lables,
+	  datasets: [
+		{
+		  label: 'エアコン使用率[%]',
+		  hoverBackgroundColor: "rgba(255,99,132,0.3)",
+		  data: perhour_data,
+		}
+	  ]
+	};
+
+	//「オプション設定」
+	var options = {
+	  title: {
+		display: true,
+		text: '1時間当たりのエアコン平均使用率'
+	  }
+	};
+
+	var canvas = document.getElementById('perhour');
+	var chart = new Chart(canvas, {
+
+	  type: 'line',  //グラフの種類
+	  data: perhour_data_out,  //表示するデータ
+	  options: options  //オプション設定
+
+	});
 }());
